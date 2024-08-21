42ab9939666ac529a1255bb643b57939042ba67c
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-08-21 13:09:29 +0530

    update-21/08/2024

--- application/views/backend/admin/dashboard/index.php
+++ application/views/backend/admin/dashboard/index.php
@@ -1,3 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <!-- start page title -->
 <div class="row ">
   <div class="col-xl-12">
@@ -28,7 +29,7 @@
                   <i class="fa-solid fa-graduation-cap" style="color: #ffffff;"></i>
                   <span style="color: #ffffff;"><?php echo get_phrase('students'); ?></span>
                   <a href="<?php echo route('student'); ?>" style="color: #6c757d; display: none;" id="student_list">
-                    <i class="mdi mdi-export"></i>
+                    <i class="mdi mdi-export text-light"></i>
                   </a>
                 </h5>
                 <h3 class="mt-3 mb-3" style="color: #ffffff;">
@@ -47,15 +48,16 @@
 
           <div class="col-lg-6">
             <div class="card widget-flat" id="teacher" style="on">
-              <div class="card-body" style="background-color:#FD858F; color: #ffffff!important;">
+              <div class="card-body" style="background-color:#FD858F; color: #ffffff;">
                 <div class="float-end">
-                  <i class="mdi mdi-account-multiple widget-icon" style="color: #ffffff;"></i>
+                  <i class="mdi mdi-account-multiple widget-icon" style="color: #ffffff!important;"></i>
                 </div>
-                <h5 class="text-muted font-weight-normal mt-0" title="Number of Teacher" style="color: #ffffff;">
+                <h5 class="text-muted font-weight-normal mt-0" title="Number of Teacher"
+                  style="color:#ffffff !important;">
                   <i class="fa-solid fa-chalkboard-user" style="color: #ffffff;"></i>
                   <?php echo get_phrase('teacher'); ?>
                   <a href="<?php echo route('teacher'); ?>" style="color: #6c757d; display: none;" id="teacher_list">
-                    <i class="mdi mdi-export"></i>
+                    <i class="mdi mdi-export text-light"></i>
                   </a>
                 </h5>
                 <h3 class="mt-3 mb-3" style="color: #ffffff;">
@@ -82,11 +84,12 @@
                 <div class="float-end">
                   <i class="mdi mdi-account-multiple widget-icon" style="color: #ffffff;"></i>
                 </div>
-                <h5 class="text-muted font-weight-normal mt-0" title="Number of Parents" style="color: #ffffff;">
+                <h5 class="text-muted font-weight-normal mt-0" title="Number of Parents"
+                  style="color: #ffffff!important;">
                   <i class="mdi mdi-account-group title_icon" style="color: #ffffff;"></i>
                   <?php echo get_phrase('parents'); ?>
                   <a href="<?php echo route('parent'); ?>" style="color: #6c757d; display: none;" id="parent_list">
-                    <i class="mdi mdi-export"></i>
+                    <i class="mdi mdi-export text-light"></i>
                   </a>
                 </h5>
                 <h3 class="mt-3 mb-3" style="color: #ffffff;">
@@ -107,12 +110,13 @@
 
           <div class="col-lg-6">
             <div class="card widget-flat">
-              <div class="card-body" style="background-color: #FF8911; color: #ffffff!important;">
+              <div class="card-body" style="background-color: #FF8911; color: #ffffff;">
                 <div class="float-end">
                   <i class="mdi mdi-account-multiple widget-icon" style="color: #ffffff;"></i>
                 </div>
-                <h5 class="text-muted font-weight-normal mt-0" title="Number of Staff" style="color: #ffffff;">
-                  <i class="mdi mdi-account-group title_icon" style="color: #ffffff;"></i>
+                <h5 class="text-muted font-weight-normal mt-0" title="Number of Staff"
+                  style="color:#ffffff !important;">
+                  <i class="mdi mdi-account-group title_icon" style="color: #ffffff!important;"></i>
                   <?php echo get_phrase('staff'); ?>
                 </h5>
                 <h3 class="mt-3 mb-3" style="color: #ffffff;">
@@ -156,7 +160,8 @@
         <div class="card">
           <div class="card-body">
             <h4 class="header-title"><?php echo get_phrase('recent_events'); ?><a
-                href="<?php echo route('event_calendar'); ?>" style="color: #6c757d;"><i class="mdi mdi-export"></i></a>
+                href="<?php echo route('event_calendar'); ?>" style="color: #6c757d;"><i
+                  class="mdi mdi-export text-light"></i></a>
             </h4>
             <?php include 'event.php'; ?>
           </div>
@@ -166,6 +171,21 @@
   </div><!-- end col-->
 </div>
 
+<!--CHART OPEN-->
+<div class="row ">
+  <div class="col-sm-6">
+    <canvas id="myLineChart"></canvas>
+  </div>
+  <div class="col-sm-6">
+
+  </div>
+
+</div>
+
+
+<!--CHART CLOSE-->
+
+
 <div class="row">
   <div class="col-xl-12">
     <div class="row">
@@ -173,7 +193,8 @@
         <div class="card">
           <div class="card-body">
             <h4 class="header-title mb-3"><?php echo get_phrase('accounts_of'); ?> <?php echo date('F'); ?> <a
-                href="<?php echo route('invoice'); ?>" style="color: #6c757d"><i class="mdi mdi-export"></i></a></h4>
+                href="<?php echo route('invoice'); ?>" style="color: #6c757d"><i
+                  class="mdi mdi-export text-light"></i></a></h4>
             <?php include 'invoice.php'; ?>
           </div>
         </div>
@@ -182,7 +203,8 @@
         <div class="card">
           <div class="card-body">
             <h4 class="header-title mb-3"> <?php echo get_phrase('expense_of'); ?> <?php echo date('F'); ?> <a
-                href="<?php echo route('expense'); ?>" style="color: #6c757d"><i class="mdi mdi-export"></i></a></h4>
+                href="<?php echo route('expense'); ?>" style="color: #6c757d"><i
+                  class="mdi mdi-export text-light"></i></a></h4>
             <?php include 'expense.php'; ?>
           </div>
         </div>
@@ -203,4 +225,59 @@
     var id = $(this).attr('id');
     $('#' + id + '_list').hide();
   });
+
+  //lINE CHART START//
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      },
+      {
+        label: 'Dataset 2',
+        data: [28, 48, 40, 19, 86, 27, 90],
+        fill: false,
+        borderColor: 'rgb(255, 99, 132)',
+        tension: 0.1
+      },
+      {
+        label: 'Dataset 3',
+        data: [18, 12, 60, 20, 46, 77, 80],
+        fill: false,
+        borderColor: 'rgb(54, 162, 235)',
+        tension: 0.1
+      }
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'SOURAV'
+        }
+      }
+    },
+  };
+
+  const myLineChart = new Chart(
+    document.getElementById('myLineChart'),
+    config
+  );
+
+  //lINE CHART END//
+
+
 </script>
