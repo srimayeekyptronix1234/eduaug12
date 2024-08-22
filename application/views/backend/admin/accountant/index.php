c08239a06784a87ae1b8de56f5433bf0227f9771
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-08-22 19:42:41 +0530

    UI Update

--- application/views/backend/admin/accountant/index.php
+++ application/views/backend/admin/accountant/index.php
@@ -1,20 +1,70 @@
+<style>
+  .title_icon {
+    font-size: 1.5rem;
+    color: #ff7580;
+    vertical-align: middle;
+  }
+
+  .page-title {
+    font-size: 20px;
+    font-weight: 700;
+    color: #ff7580;
+    line-height: 1.5;
+  }
+
+  .ms-2 {
+    margin-left: 0.5rem;
+  }
+
+  .parentbar {
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    background: #ffdfe8;
+  }
+
+  .inspiring-line {
+    font-size: 16px;
+    color: #2c2c2c !important;
+  }
+
+  .parbox {
+    font-weight: 600;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 10px;
+  }
+
+  .parbox:hover {
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    transition: 1s ease;
+  }
+</style>
+
 <!--title-->
-<div class="row ">
+<div class="row">
   <div class="col-xl-12">
-    <div class="card">
-      <div class="card-body py-2">
-        <h4 class="page-title d-inline-block">
-          <i class="mdi mdi-account-circle title_icon"></i> <?php echo get_phrase('accountant'); ?>
-        </h4>
-        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1" onclick="rightModal('<?php echo site_url('modal/popup/accountant/create'); ?>', '<?php echo get_phrase('create_accountant'); ?>')"> <i class="mdi mdi-plus"></i> <?php echo get_phrase('add_accountant'); ?></button>
+    <div class="card parentbar">
+      <div class="card-body py-2 d-flex justify-content-between align-items-center">
+        <div>
+          <h4 class="page-title d-flex align-items-center">
+            <i class="mdi mdi-account-circle title_icon"></i>
+            <span class="ms-2"><?php echo get_phrase('accountant'); ?></span>
+          </h4>
+          <p class="inspiring-line mt-2 ">
+            Managing finances with precision and integrity.
+          </p>
+        </div>
+        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1"
+          onclick="rightModal('<?php echo site_url('modal/popup/accountant/create'); ?>', '<?php echo get_phrase('create_accountant'); ?>')">
+          <i class="mdi mdi-plus"></i> <?php echo get_phrase('add_accountant'); ?>
+        </button>
       </div> <!-- end card body-->
     </div> <!-- end card -->
   </div><!-- end col-->
 </div>
 
+
 <div class="row">
   <div class="col-12">
-    <div class="card">
+    <div class="card parbox">
       <div class="card-body accountant_content">
         <?php include 'list.php'; ?>
       </div>
@@ -23,16 +73,16 @@
 </div>
 
 <script>
-var showAllAccountants = function () {
-  var url = '<?php echo route('accountant/list'); ?>';
+  var showAllAccountants = function () {
+    var url = '<?php echo route('accountant/list'); ?>';
 
-  $.ajax({
-    type : 'GET',
-    url: url,
-    success : function(response) {
-      $('.accountant_content').html(response);
-      initDataTable('basic-datatable');
-    }
-  });
-}
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function (response) {
+        $('.accountant_content').html(response);
+        initDataTable('basic-datatable');
+      }
+    });
+  }
 </script>
--- application/views/backend/admin/users_list/index.php
+++ application/views/backend/admin/users_list/index.php
@@ -1,20 +1,71 @@
+<style>
+  .title_icon {
+    font-size: 1.5rem;
+    color: #ff7580;
+    vertical-align: middle;
+  }
+
+  .page-title {
+    font-size: 20px;
+    font-weight: 700;
+    color: #ff7580;
+    line-height: 1.5;
+  }
+
+  .ms-2 {
+    margin-left: 0.5rem;
+  }
+
+  .parentbar {
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    background: #ffdfe8;
+  }
+
+  .inspiring-line {
+    font-size: 16px;
+    color: #2c2c2c !important;
+    font-weight: 600;
+  }
+
+  .parbox {
+    font-weight: 600;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 10px;
+  }
+
+  .parbox:hover {
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    transition: 1s ease;
+  }
+</style>
+
 <!--title-->
-<div class="row ">
+<div class="row">
   <div class="col-xl-12">
-    <div class="card">
-      <div class="card-body py-2">
-        <h4 class="page-title d-inline-block">
-          <i class="mdi mdi-account-circle title_icon"></i> <?php echo get_phrase('user'); ?>
-        </h4>
-        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1" onclick="rightModal('<?php echo site_url('modal/popup/users_list/create'); ?>', '<?php echo get_phrase('create_HR'); ?>')"> <i class="mdi mdi-plus"></i> <?php echo get_phrase('create_HR'); ?></button>
+    <div class="card parentbar">
+      <div class="card-body py-2 d-flex justify-content-between align-items-center">
+        <div>
+          <h4 class="page-title d-flex align-items-center">
+            <i class="mdi mdi-account-circle title_icon"></i>
+            <span class="ms-2"><?php echo get_phrase('user'); ?></span>
+          </h4>
+          <p class="inspiring-line mt-2">
+            Empowering your team with tools for success.
+          </p>
+        </div>
+        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1"
+          onclick="rightModal('<?php echo site_url('modal/popup/users_list/create'); ?>', '<?php echo get_phrase('create_HR'); ?>')">
+          <i class="mdi mdi-plus"></i> <?php echo get_phrase('create_HR'); ?>
+        </button>
       </div> <!-- end card body-->
     </div> <!-- end card -->
   </div><!-- end col-->
 </div>
 
+
 <div class="row">
   <div class="col-12">
-    <div class="card">
+    <div class="card parbox">
       <div class="card-body user_content">
         <?php include 'list.php'; ?>
       </div>
@@ -23,19 +74,19 @@
 </div>
 
 <script>
-var showAllUsers = function () {
-  var url = '<?php echo route('users_list/list'); ?>';
+  var showAllUsers = function () {
+    var url = '<?php echo route('users_list/list'); ?>';
 
-  $.ajax({
-    type : 'GET',
-    url: url,
-    success : function(response) {
-      // Hide the loader
-      document.getElementById('loader').style.display = 'none';
-      document.getElementById('submitbtn').disabled = false;
-      $('.user_content').html(response);
-      initDataTable('basic-datatable');
-    }
-  });
-}
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function (response) {
+        // Hide the loader
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('submitbtn').disabled = false;
+        $('.user_content').html(response);
+        initDataTable('basic-datatable');
+      }
+    });
+  }
 </script>
