8cafa47141aca99f37cac2f5e0d6d56e59ca4e1c
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-08-23 15:15:02 +0530

    UI Update

--- application/views/backend/admin/assignvehicle/index.php
+++ application/views/backend/admin/assignvehicle/index.php
@@ -1,37 +1,95 @@
+<style>
+  .title_icon {
+    font-size: 1.5rem;
+    color: #ff7580;
+    vertical-align: middle;
+  }
+
+  .page-title {
+    font-size: 20px;
+    font-weight: 700;
+    color: #ff7580;
+    line-height: 1.5;
+  }
+
+  .ms-2 {
+    margin-left: 0.5rem;
+  }
+
+  .parentbar {
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    background: #ffdfe8;
+  }
+
+  .inspiring-line {
+    font-size: 16px;
+    color: #2c2c2c !important;
+    font-weight: 600;
+  }
+
+  .parbox {
+    font-weight: 600;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    border-radius: 10px;
+    padding: 20PX;
+  }
+
+  .parbox:hover {
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    transition: 1s ease;
+  }
+
+  .boxbtn {
+    background: #0272f3;
+    color: white;
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+</style>
+
 <!--title-->
-<div class="row ">
+<div class="row">
   <div class="col-xl-12">
-    <div class="card">
-      <div class="card-body py-2">
-        <h4 class="page-title d-inline-block">
-          <i class="mdi mdi-car-estate title_icon"></i> <?php echo get_phrase('assign vehicle'); ?>
-        </h4>
-        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1" onclick="rightModal('<?php echo site_url('modal/popup/assignvehicle/create'); ?>', '<?php echo get_phrase('assign_vehicle'); ?>')"> <i class="mdi mdi-plus"></i> <?php echo get_phrase('assign_vehicle'); ?></button>
+    <div class="card parentbar">
+      <div class="card-body py-2 d-flex justify-content-between align-items-center">
+        <div>
+          <h4 class="page-title d-inline-block">
+            <i class="mdi mdi-car-estate title_icon"></i> <?php echo get_phrase('assign_vehicle'); ?>
+          </h4>
+          <p class="inspiring-line">
+            Seamlessly connecting your resources to their destinations.
+          </p>
+        </div>
+        <button type="button" class="btn btn-outline-primary btn-rounded alignToTitle float-end mt-1"
+          onclick="rightModal('<?php echo site_url('modal/popup/assignvehicle/create'); ?>', '<?php echo get_phrase('assign_vehicle'); ?>')">
+          <i class="mdi mdi-plus"></i> <?php echo get_phrase('assign_vehicle'); ?>
+        </button>
       </div> <!-- end card body-->
     </div> <!-- end card -->
   </div><!-- end col-->
 </div>
+
 <div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body assign_vehicle_content">
-                <?php include 'list.php'; ?>
-            
-           </div>
-</div>
-</div>
+  <div class="col-12">
+    <div class="card parbox">
+      <div class="card-body assign_vehicle_content">
+        <?php include 'list.php'; ?>
+
+      </div>
+    </div>
+  </div>
 </div>
 
 <script>
 
-var showAllAssignVehicle = function () {
-        $.ajax({
-            url: '<?php echo route('assignvehicle/list/') ?>',
-            success: function(response){
-                $('.assign_vehicle_content').html(response);
-            }
-        });
-    
-}
+  var showAllAssignVehicle = function () {
+    $.ajax({
+      url: '<?php echo route('assignvehicle/list/') ?>',
+      success: function (response) {
+        $('.assign_vehicle_content').html(response);
+      }
+    });
+
+  }
 
 </script>
--- application/views/backend/admin/vehicle/list.php
+++ application/views/backend/admin/vehicle/list.php
@@ -1,12 +1,19 @@
-<?php
-    
-    $check_data = $this->db->get('vehicle')->result_array();
+<style>
+  .filbtn:hover {
+    background: #0272f3;
+    color: white;
+  }
+</style>
 
-   
-  if (count($check_data) > 0):?>
+<?php
+
+$check_data = $this->db->get('vehicle')->result_array();
+
+
+if (count($check_data) > 0): ?>
   <table id="basic-datatable" class="table table-striped dt-responsive nowrap" width="100%">
     <thead>
-      <tr style="background-color: #313a46; color: #ababab;">
+      <tr style="background-color: #0272F3; color: #fff;">
         <th><?php echo get_phrase('Vehicle Number'); ?></th>
         <th><?php echo get_phrase('Vehicle Model'); ?></th>
         <th><?php echo get_phrase('Driver Name'); ?></th>
@@ -15,24 +22,28 @@
     </thead>
     <tbody>
       <?php
-      foreach($check_data as $data){
-        
+      foreach ($check_data as $data) {
+
         ?>
         <tr>
           <td><?php echo $data['vehicle_number']; ?></td>
           <td><?php echo $data['vehicle_model']; ?></td>
           <td><?php echo $data['vehicle_driver']; ?></td>
-       
+
           <td>
 
             <div class="dropdown text-center">
-              <button type="button" class="btn btn-sm btn-icon btn-rounded btn-outline-secondary dropdown-btn dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false"><i class="mdi mdi-dots-vertical"></i></button>
+              <button type="button"
+                class="btn btn-sm btn-icon btn-rounded btn-outline-secondary dropdown-btn dropdown-toggle arrow-none card-drop filbtn"
+                data-bs-toggle="dropdown" aria-expanded="false"><i class="mdi mdi-dots-vertical"></i></button>
               <div class="dropdown-menu dropdown-menu-end">
                 <!-- item-->
-                 <a href="javascript:void(0);" class="dropdown-item" onclick="rightModal('<?php echo site_url('modal/popup/vehicle/edit/'.$data['id'])?>', '<?php echo get_phrase('update_vehicle'); ?>');"><?php echo get_phrase('edit'); ?></a>
+                <a href="javascript:void(0);" class="dropdown-item"
+                  onclick="rightModal('<?php echo site_url('modal/popup/vehicle/edit/' . $data['id']) ?>', '<?php echo get_phrase('update_vehicle'); ?>');"><?php echo get_phrase('edit'); ?></a>
                 <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item" onclick="confirmModal('<?php echo route('vehicle/delete/'.$data['id']); ?>', showAllVehicle)"><?php echo get_phrase('delete'); ?></a>
-                                              
+                <a href="javascript:void(0);" class="dropdown-item"
+                  onclick="confirmModal('<?php echo route('vehicle/delete/' . $data['id']); ?>', showAllVehicle)"><?php echo get_phrase('delete'); ?></a>
+
               </div>
             </div>
           </td>
@@ -41,5 +52,5 @@
     </tbody>
   </table>
 <?php else: ?>
-  <?php include APPPATH.'views/backend/empty.php'; ?>
+  <?php include APPPATH . 'views/backend/empty.php'; ?>
 <?php endif; ?>
