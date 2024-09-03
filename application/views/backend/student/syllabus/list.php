b56b91a84cc119c0202e19b677c054dbadd759ad
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-09-03 19:26:53 +0530

    ui update

--- application/views/backend/student/syllabus/list.php
+++ application/views/backend/student/syllabus/list.php
@@ -1,29 +1,45 @@
+<style>
+    .btn-check:focus+.btn-info,
+    .btn-info {
+        background: #013A7C !important;
+        border: none;
+    }
+
+    .btn-check:focus+.btn-info,
+    .btn-info:hover {
+        background: #000 !important;
+        border: none;
+    }
+</style>
+
 <?php
 $school_id = school_id();
 if (isset($class_id) && isset($section_id)):
     $syllabuses = $this->db->get_where('syllabuses', array('class_id' => $class_id, 'section_id' => $section_id, 'session_id' => active_session()))->result_array();
-    if(count($syllabuses) > 0):?>
-    <table id="basic-datatable" class="table table-striped dt-responsive nowrap" width="100%">
-        <thead>
-            <tr style="background-color: #313a46; color: #ababab;">
-                <th><?php echo get_phrase('title'); ?></th>
-                <th><?php echo get_phrase('syllabus'); ?></th>
-                <th><?php echo get_phrase('subject'); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($syllabuses as $syllabus):?>
-                <tr>
-                    <td><?php echo $syllabus['title']; ?></td>
-                    <td><a href="<?php echo base_url('uploads/syllabus/'.$syllabus['file']); ?>" class="btn btn-info mdi mdi-download" download><?php echo get_phrase('download'); ?></a></td>
-                    <td><?php echo $this->db->get_where('subjects', array('id' => $syllabus['subject_id']))->row('name'); ?></td>
+    if (count($syllabuses) > 0): ?>
+        <table id="basic-datatable" class="table table-striped dt-responsive nowrap" width="100%">
+            <thead>
+                <tr style="background-color: #0272F3; color: #fff;">
+                    <th><?php echo get_phrase('title'); ?></th>
+                    <th><?php echo get_phrase('syllabus'); ?></th>
+                    <th><?php echo get_phrase('subject'); ?></th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($syllabuses as $syllabus): ?>
+                    <tr>
+                        <td><?php echo $syllabus['title']; ?></td>
+                        <td><a href="<?php echo base_url('uploads/syllabus/' . $syllabus['file']); ?>"
+                                class="btn btn-info mdi mdi-download" download><?php echo get_phrase('download'); ?></a></td>
+                        <td><?php echo $this->db->get_where('subjects', array('id' => $syllabus['subject_id']))->row('name'); ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     <?php else: ?>
-        <?php include APPPATH.'views/backend/empty.php'; ?>
+        <?php include APPPATH . 'views/backend/empty.php'; ?>
     <?php endif; ?>
 <?php else: ?>
-    <?php include APPPATH.'views/backend/empty.php'; ?>
+    <?php include APPPATH . 'views/backend/empty.php'; ?>
 <?php endif; ?>
