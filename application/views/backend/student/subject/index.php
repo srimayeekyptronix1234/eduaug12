5376130e231896d72c564a77910b89ea2dd07a15
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-09-03 19:09:28 +0530

    UI update

--- application/views/backend/student/subject/index.php
+++ application/views/backend/student/subject/index.php
@@ -1,28 +1,168 @@
+<style>
+    .progress {
+        margin: 20px auto;
+        padding: 0;
+        width: 90%;
+        background: #e5e5e5;
+        border-radius: 6px;
+        position: relative;
+    }
+
+    p {
+        text-align: left;
+        text-transform: capitalize;
+        font-weight: 500;
+    }
+
+    .progress-header {
+        text-align: center;
+        margin-bottom: 5px;
+        font-size: 14px;
+        color: black;
+    }
+
+    .bar-container {
+        position: relative;
+        width: 100%;
+        height: 30px;
+        background: #e5e5e5;
+        border-radius: 6px;
+        overflow: hidden;
+    }
+
+    .bar {
+        height: 100%;
+        background: cornflowerblue;
+        border-radius: 6px 0 0 6px;
+    }
+
+    .percent-outside {
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+        margin: 0;
+        font-family: tahoma, arial, helvetica;
+        font-size: 12px;
+        color: black;
+    }
+
+    .bar-box {
+        background: #fff;
+        border-radius: 10px;
+        padding: 20px 20px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    }
+
+    .bar-box:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .linebar {
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+        margin-bottom: 20px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    }
+
+    .linebar:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .boxhover {
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        border-radius: 10px;
+    }
+
+    .boxhover:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .colhover {
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+        border-radius: 10px;
+    }
+
+    .colhover:hover {
+        box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+        transition: 1s ease;
+    }
+
+    #myLineChart {
+        width: 100% !important;
+    }
+
+    .adminbar {
+        background: #ffdfe8;
+        border-radius: 10px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+    }
+
+    .table-responsive {
+        display: revert !important;
+    }
+
+    .card {
+        border: none !important;
+    }
+
+    .boxbtn {
+        background: #0272F3;
+        color: #fff;
+    }
+
+    .boxbtn:hover {
+        background: #000;
+        color: #fff;
+    }
+</style>
+
 <?php $student_data = $this->user_model->get_logged_in_student_details(); ?>
 <!--title-->
-<div class="row ">
-  <div class="col-xl-12">
-    <div class="card">
-      <div class="card-body py-2">
-        <h4 class="page-title d-inline-block"><i class="mdi mdi-book-open-page-variant title_icon"></i> <?php echo get_phrase('subject'); ?></h4>
-      </div> <!-- end card body-->
-    </div> <!-- end card -->
-  </div><!-- end col-->
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-body d-flex align-items-center justify-content-between adminbar py-2">
+                <div>
+                    <h4 class="page-title mb-0 d-flex align-items-center">
+                        <i class="mdi mdi-book-open-page-variant title_icon"
+                            style="font-size: 1.5rem; color: #ff7580;"></i>
+                        <span
+                            style="font-size: 1.5rem; font-weight: 700; color: #ff7580; margin-left: 10px; text-transform: capitalize;">
+                            <?php echo get_phrase('subject'); ?>
+                        </span>
+                    </h4>
+                    <p class="text-muted mt-2" style="font-size: 16px; font-weight: 600; color: #2c2c2c !important;">
+                        Explore all the subjects offered in this semester.
+                    </p>
+                </div>
+            </div> <!-- end card body-->
+        </div> <!-- end card -->
+    </div><!-- end col-->
 </div>
+
 
 <div class="row">
     <div class="col-12">
-        <div class="card">
+        <div class="card boxhover">
             <div class="row mt-3">
                 <div class="col-md-3"></div>
                 <div class="col-md-4">
-                    <select name="class_id" id="class_id" class="form-control select2" data-toggle = "select2" required>
+                    <select name="class_id" id="class_id" class="form-control select2" data-toggle="select2" required>
                         <option value=""><?php echo get_phrase('select_a_class'); ?></option>
-                        <option value="<?php echo $student_data['class_id']; ?>"><?php echo $student_data['class_name']; ?></option>
+                        <option value="<?php echo $student_data['class_id']; ?>">
+                            <?php echo $student_data['class_name']; ?>
+                        </option>
                     </select>
                 </div>
                 <div class="col-md-2">
-                    <button class="btn btn-block btn-secondary" onclick="filter_class()" ><?php echo get_phrase('filter'); ?></button>
+                    <button class="btn btn-block boxbtn"
+                        onclick="filter_class()"><?php echo get_phrase('filter'); ?></button>
                 </div>
             </div>
             <div class="card-body subject_content">
@@ -34,24 +174,24 @@
 
 
 <script>
-function filter_class(){
-    var class_id = $('#class_id').val();
-    if(class_id != ""){
-        showAllSubjects();
-    }else{
-        toastr.error('<?php echo get_phrase('please_select_a_class'); ?>');
+    function filter_class() {
+        var class_id = $('#class_id').val();
+        if (class_id != "") {
+            showAllSubjects();
+        } else {
+            toastr.error('<?php echo get_phrase('please_select_a_class'); ?>');
+        }
     }
-}
 
-var showAllSubjects = function () {
-    var class_id = $('#class_id').val();
-    if(class_id != ""){
-        $.ajax({
-            url: '<?php echo route('subject/list/') ?>'+class_id,
-            success: function(response){
-                $('.subject_content').html(response);
-            }
-        });
+    var showAllSubjects = function () {
+        var class_id = $('#class_id').val();
+        if (class_id != "") {
+            $.ajax({
+                url: '<?php echo route('subject/list/') ?>' + class_id,
+                success: function (response) {
+                    $('.subject_content').html(response);
+                }
+            });
+        }
     }
-}
 </script>
