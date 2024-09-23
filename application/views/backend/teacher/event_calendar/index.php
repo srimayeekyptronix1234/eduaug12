ecd7286e5969afd3def447a4018ef32a93186a06
sourav-kyptronix <sourav06.kyptronix@gmail.com>
2024-09-23 14:15:47 +0530

    UI Update

--- application/views/backend/teacher/event_calendar/index.php
+++ application/views/backend/teacher/event_calendar/index.php
@@ -1,15 +1,147 @@
+<style>
+    .progress {
+        margin: 20px auto;
+        padding: 0;
+        width: 90%;
+        background: #e5e5e5;
+        border-radius: 6px;
+        position: relative;
+    }
+
+    p {
+        text-align: left;
+        text-transform: capitalize;
+        font-weight: 500;
+    }
+
+    .progress-header {
+        text-align: center;
+        margin-bottom: 5px;
+        font-size: 14px;
+        color: black;
+    }
+
+    .bar-container {
+        position: relative;
+        width: 100%;
+        height: 30px;
+        background: #e5e5e5;
+        border-radius: 6px;
+        overflow: hidden;
+    }
+
+    .bar {
+        height: 100%;
+        background: cornflowerblue;
+        border-radius: 6px 0 0 6px;
+    }
+
+    .percent-outside {
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        transform: translateY(-50%);
+        margin: 0;
+        font-family: tahoma, arial, helvetica;
+        font-size: 12px;
+        color: black;
+    }
+
+    .bar-box {
+        background: #fff;
+        border-radius: 10px;
+        padding: 20px 20px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    }
+
+    .bar-box:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .linebar {
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+        margin-bottom: 20px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    }
+
+    .linebar:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .boxhover {
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        border-radius: 10px;
+    }
+
+    .boxhover:hover {
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        transition: 0.3s ease;
+    }
+
+    .colhover {
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+        border-radius: 10px;
+    }
+
+    .colhover:hover {
+        box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+        transition: 1s ease;
+    }
+
+    #myLineChart {
+        width: 100% !important;
+    }
+
+    .adminbar {
+        background: #ffdfe8;
+        border-radius: 10px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+    }
+
+    .table-responsive {
+        display: revert !important;
+    }
+
+    .card {
+        border: none !important;
+    }
+
+    .filbtn {
+        color: #fff;
+        background-color: #00398f;
+        border-color: #00398f;
+        padding: 6px 50px;
+    }
+</style>
+
 <!--title-->
-<div class="row ">
+<div class="row">
     <div class="col-xl-12">
-        <div class="card">
-            <div class="card-body">
-                <h4 class="page-title">
-                    <i class="mdi mdi-calendar-clock title_icon"></i> <?php echo get_phrase('event_calendar'); ?>
-                </h4>
+        <div class="card adminbar">
+            <div class="card-body d-flex align-items-center justify-content-between py-2">
+                <div>
+                    <h4 class="page-title mb-0 d-flex align-items-center">
+                        <i class="mdi mdi-calendar-clock title_icon" style="font-size: 1.5rem; color: #ff7580;"></i>
+                        <span
+                            style="font-size: 1.5rem; font-weight: 700; color: #ff7580; margin-left: 10px; text-transform: capitalize;">
+                            <?php echo get_phrase('event_calendar'); ?>
+                        </span>
+                    </h4>
+                    <p class="text-muted mt-2" style="font-size: 16px; font-weight: 600; color: #2c2c2c !important;">
+                        Stay updated with upcoming events and activities
+                    </p>
+                </div>
             </div> <!-- end card body-->
         </div> <!-- end card -->
     </div><!-- end col-->
 </div>
+
+
 <div class="row">
     <div class="col-12 event_calendar_content">
         <?php include 'list.php'; ?>
@@ -17,38 +149,38 @@
 </div>
 
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         refreshEventCalendar();
     });
 
     var showAllEvents = function () {
-       var url = '<?php echo route('event_calendar/list'); ?>';
+        var url = '<?php echo route('event_calendar/list'); ?>';
 
-       $.ajax({
-           type : 'GET',
-           url: url,
-           success : function(response) {
-               $('.event_calendar_content').html(response);
-               initDataTable("basic-datatable");
-               refreshEventCalendar();
-           }
-       });
-   }
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function (response) {
+                $('.event_calendar_content').html(response);
+                initDataTable("basic-datatable");
+                refreshEventCalendar();
+            }
+        });
+    }
 
     var refreshEventCalendar = function () {
         var url = '<?php echo route('event_calendar/all_events'); ?>';
 
         $.ajax({
-            type : 'GET',
+            type: 'GET',
             url: url,
             dataType: 'json',
-            success : function(response) {
+            success: function (response) {
 
                 var event_calendar = [];
-                for(let i = 0; i < response.length; i++) {
+                for (let i = 0; i < response.length; i++) {
 
                     var obj;
-                    obj  = {"title" : response[i].title, "start" : response[i].starting_date, "end" : response[i].ending_date};
+                    obj = { "title": response[i].title, "start": response[i].starting_date, "end": response[i].ending_date };
                     event_calendar.push(obj);
                 }
 
