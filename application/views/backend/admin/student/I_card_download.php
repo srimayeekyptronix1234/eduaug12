28d4edc2a782befd48a08d525683321eac04f169
bikashghosh2807 <bikashghosh2807@gmail.com>
2024-10-30 20:05:27 +0530

    file upload final 30-10-2024

--- application/views/backend/admin/student/I_card_download.php
+++ application/views/backend/admin/student/I_card_download.php
@@ -1,10 +1,62 @@
 <?php
 $user_details = $this->db->get_where('users', array('id' => $param1))->row_array();
+
+$school_id = school_id();
+$icard_details = $this->db->get_where('i_card_setting', array('school_id' => $school_id))->row_array();
 ?>
 
 <div class="card-container" style="display: flex; gap: 20px; flex-wrap: wrap; justify-content: center;">
 
-<div class="id-card-holder" id="idCard" style="width: 280px; padding: 4px; background-color: #1f1f1f; border-radius: 5px; position: relative; text-align: center;">
+    <!-- Card 1 -->
+        <div class="id-card-holder" id="idCard1" style="width: 280px; padding: 4px; background-color: #1f1f1f; border-radius: 5px; position: relative; text-align: center; <?php if($icard_details['i_card_template_no'] == 1){?> display:block; <?php } else {?> display:none; <?php } ?>">
+          <div class="id-card" style="background-image: url('<?php echo site_url(); ?>uploads/ICardImages/1.jpg'); background-size: cover; background-position: center; padding: 15px; border-radius: 10px; text-align: center; box-shadow: 0 0 1.5px 0px #b9b9b9; position: relative; overflow: hidden;">
+            <div class="header">
+              <img src="<?php echo site_url(); ?>uploads/ICardImages/logo.png" alt="University Logo" style="width: 90px;" />
+            </div>
+            <div class="photo">
+              <label for="card1">
+                <img src="<?php echo site_url(); ?>uploads/ICardImages/user.jpeg" alt="Student Photo" style="border-radius: 5%; border: 3px solid #41686b;width: 100px; margin: 6px;" />
+              </label>
+            </div>
+            <h2 style="font-size: 18px; margin: 5px 0; position: relative; z-index: 2; color: #000;">Prabsaran Singh</h2>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Student</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Class-VI</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">BT19GCS121</h3>
+            <hr style="border: 1px dashed #0000003b;">
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;"><strong>Eduquest University</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">3068 Conaway Street, Houston.</p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Texas, United States <strong>301705</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Ph: 01494-660600, 7073222393</p>
+          </div>
+          
+        </div>
+
+        <!-- Card 2 -->
+        <div class="id-card-holder" id="idCard2" style="width: 280px; padding: 4px; background-color: #1f1f1f; border-radius: 5px; position: relative; text-align: center; <?php if($icard_details['i_card_template_no'] == 2){?> display:block; <?php } else {?> display:none; <?php } ?>">
+          <div class="id-card" style="background-image: url('<?php echo site_url(); ?>uploads/ICardImages/2.jpg'); background-size: cover; background-position: center; padding: 15px; border-radius: 10px; text-align: center; box-shadow: 0 0 1.5px 0px #b9b9b9; position: relative; overflow: hidden;">
+            <div class="header">
+              <img src="<?php echo site_url(); ?>uploads/ICardImages/logo.png" alt="University Logo" style="width: 90px;" />
+            </div>
+            <div class="photo">
+              <label for="card2">
+                <img src="<?php echo site_url(); ?>uploads/ICardImages/user.jpeg" alt="Student Photo" style="border-radius: 5%; border: 3px solid #41686b; width: 100px; margin: 6px;" />
+              </label>
+            </div>
+            <h2 style="font-size: 18px; margin: 5px 0; position: relative; z-index: 2; color: #000;">Alex Johnson</h2>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Student</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Class-VII</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">BT19GCS122</h3>
+            <hr style="border: 1px dashed #0000003b;">
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;"><strong>Eduquest University</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">3068 Conaway Street, Houston.</p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Texas, United States <strong>301705</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Ph: 01494-660600, 7073222393</p>
+          </div>
+          
+        </div>
+
+        <!-- Card 3 -->
+        <div class="id-card-holder" id="idCard3" style="width: 280px; padding: 4px; background-color: #1f1f1f; border-radius: 5px; position: relative; text-align: center; <?php if($icard_details['i_card_template_no'] == 3){?> display:block; <?php } else {?> display:none; <?php } ?>">
           <div class="id-card" style="background-image: url('<?php echo site_url(); ?>uploads/ICardImages/3.jpg'); background-size: cover; background-position: center; padding: 15px; border-radius: 10px; text-align: center; box-shadow: 0 0 1.5px 0px #b9b9b9; position: relative; overflow: hidden;">
             <div class="header">
               <img src="<?php echo site_url(); ?>uploads/ICardImages/logo.png" alt="University Logo" style="width: 90px;" />
@@ -27,9 +79,33 @@ $user_details = $this->db->get_where('users', array('id' => $param1))->row_array
           
         </div>
 
+        <!-- Card 4 -->
+        <div class="id-card-holder" id="idCard4" style="width: 280px; padding: 4px; background-color: #1f1f1f; border-radius: 5px; position: relative; text-align: center; <?php if($icard_details['i_card_template_no'] == 4){?> display:block; <?php } else {?> display:none; <?php } ?>">
+          <div class="id-card" style="background-image: url('<?php echo site_url(); ?>uploads/ICardImages/4.jpg'); background-size: cover; background-position: center; padding: 15px; border-radius: 10px; text-align: center; box-shadow: 0 0 1.5px 0px #b9b9b9; position: relative; overflow: hidden;">
+            <div class="header">
+              <img src="<?php echo site_url(); ?>uploads/ICardImages/logo.png" alt="University Logo" style="width: 90px;" />
+            </div>
+            <div class="photo">
+              <label for="card4">
+                <img src="<?php echo site_url(); ?>uploads/ICardImages/user.jpeg" alt="Student Photo" style="border-radius: 5%; border: 3px solid #41686b; width: 100px; margin: 6px;" />
+              </label>
+            </div>
+            <h2 style="font-size: 18px; margin: 5px 0; position: relative; z-index: 2; color: #000;">John Doe</h2>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Student</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">Class-IX</h3>
+            <h3 style="font-size: 14px; margin: 5.5px 0; font-weight: 500; position: relative; z-index: 2; color: #000;">BT19GCS124</h3>
+            <hr style="border: 1px dashed #0000003b;">
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;"><strong>Eduquest University</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">3068 Conaway Street, Houston.</p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Texas, United States <strong>301705</strong></p>
+            <p style="font-size: 12px; margin: 1px; position: relative; z-index: 2; color: #000;">Ph: 01494-660600, 7073222393</p>
+          </div>
+          
+        </div>
+
 </div>      
 
-<button onclick="printIDCard()">Print ID Card</button>
+<button onclick="printIDCard(<?php echo $icard_details['i_card_template_no'];?>)">Print ID Card</button>
 
 <script>
     // function printIDCard() {
@@ -41,9 +117,9 @@ $user_details = $this->db->get_where('users', array('id' => $param1))->row_array
     //     document.body.innerHTML = originalContents;
     // }
 
-    function printIDCard() {
+    function printIDCard(icardNo) {
         // Get the HTML of the card element
-        const printContents = document.getElementById('idCard').outerHTML;
+        const printContents = document.getElementById('idCard'+icardNo).outerHTML;
         
         // Create a new iframe for printing
         const iframe = document.createElement('iframe');
